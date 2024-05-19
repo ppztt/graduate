de15b67c050b743f89834e8094cea6c59363091b
彭泽涛 <pztttwl@163.com>
2024-05-19 23:39:29 +0800

    引入路由啥的

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'antd'
+
+export default function Login() {
+    return (
+        <div>
+            Login
+            <Button type='primary'>登录</Button>
+        </div>
+    )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,27 @@
-const router = ()=> {}
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import Login from "../pages/login"
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        // 重定向组件Navigate
+        element: <Navigate to={'/login'} replace />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/home',
+        element: <div>hello, home</div>
+    }
+])
+
+const Router = () => {
+    return (
+        <RouterProvider router={routes} />
+    )
+}
 
 export {
-    router
+    Router
 }
