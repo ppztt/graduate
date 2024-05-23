d5ec611e68de84370efb38259d4de1d108223670
pzt_ttwl <sx_ttwlpeng@canway.net>
2024-05-23 10:16:51 +0800

    style

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import type { FormProps } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
+import { useNavigate } from 'react-router-dom';
 import { loginForm } from '@/type/loginType';
 import './index.scss'
 
+
 const Login: React.FC = () => {
+    const navigate = useNavigate()
+
     const [loginInfo] = Form.useForm<loginForm>();
     const onFinish: FormProps<loginForm>['onFinish'] = (values) => {
         console.log('Success:', values)
+        navigate('/home/about')
     }
     
     const onFinishFailed: FormProps<loginForm>['onFinishFailed'] = (errorInfo) => {
