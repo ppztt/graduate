b4923bd82fef7c799abf5f5848dd3411c80b815b
pzt_ttwl <ttwlpeng@canway.net>
2024-10-10 11:22:09 +0800

    feat: 调整样式，增加图表根据视图更新

--- src/pages/dataCenter/components/linechart.tsx
+++ src/pages/dataCenter/components/linechart.tsx
@@ -1,70 +1,97 @@
-import React, {useEffect, useRef} from "react"
+import React, { useEffect, useRef } from "react"
 import * as echarts from 'echarts'
-const PieChart: React.FC = () => {
+import debounce from "lodash/debounce"
+import './index.scss'
+const LineChart: React.FC = () => {
     const chartEle = useRef<any>(null)
+    let handleResize: any = null
+    const init = () => {
+        try {
+            const lineChart = echarts.init(chartEle.current)
+            lineChart.setOption({
+                title: {
+                    text: ''
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: 'Email',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [120, 132, 101, 134, 90, 230, 210]
+                    },
+                    {
+                        name: 'Union Ads',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [220, 182, 191, 234, 290, 330, 310]
+                    },
+                    {
+                        name: 'Video Ads',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [150, 232, 201, 154, 190, 330, 410]
+                    },
+                    {
+                        name: 'Direct',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [320, 332, 301, 334, 390, 330, 320]
+                    },
+                    {
+                        name: 'Search Engine',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [820, 932, 901, 934, 1290, 1330, 1320]
+                    }
+                ]
+            })
+            handleResize = debounce(() => {
+                lineChart && lineChart.resize()
+            }, 160)
+            lineChart && window.addEventListener('resize', () => {
+                handleResize()
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
     useEffect(() => {
-        const pieChart = echarts.init(chartEle.current)
-        pieChart.setOption({
-            title: {
-              text: 'Stacked Line'
-            },
-            tooltip: {
-              trigger: 'axis'
-            },
-            legend: {
-              data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-            },
-            grid: {
-              left: '3%',
-              right: '4%',
-              bottom: '3%',
-              containLabel: true
-            },
-            xAxis: {
-              type: 'category',
-              boundaryGap: false,
-              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-              type: 'value'
-            },
-            series: [
-              {
-                name: 'Email',
-                type: 'line',
-                stack: 'Total',
-                data: [120, 132, 101, 134, 90, 230, 210]
-              },
-              {
-                name: 'Union Ads',
-                type: 'line',
-                stack: 'Total',
-                data: [220, 182, 191, 234, 290, 330, 310]
-              },
-              {
-                name: 'Video Ads',
-                type: 'line',
-                stack: 'Total',
-                data: [150, 232, 201, 154, 190, 330, 410]
-              },
-              {
-                name: 'Direct',
-                type: 'line',
-                stack: 'Total',
-                data: [320, 332, 301, 334, 390, 330, 320]
-              },
-              {
-                name: 'Search Engine',
-                type: 'line',
-                stack: 'Total',
-                data: [820, 932, 901, 934, 1290, 1330, 1320]
-              }
-            ]
-          });
+        try {
+            init()
+        } catch (error) {
+            console.log(error)
+        }
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
-        <div id="chart" ref={chartEle} style={{height: '100%', width: '100%'}}></div>
+        <div id="chart-component" style={{ height: '100%', width: '100%' }}>
+            <p className="title">变化趋势折线图</p>
+            <div className="chart-ele" ref={chartEle}></div>
+        </div>
     )
 }
 
-export default PieChart
+export default LineChart
--- src/pages/dataCenter/components/piechart.tsx
+++ src/pages/dataCenter/components/piechart.tsx
@@ -1,47 +1,69 @@
-import React, {useEffect, useRef} from "react"
+import React, { useEffect, useRef } from "react"
 import * as echarts from 'echarts'
+import debounce from 'lodash/debounce'
+import './index.scss'
 const PieChart: React.FC = () => {
     const chartEle = useRef<any>(null)
-    useEffect(() => {
+    let handleResize: any = null
+    const init = () => {
         const pieChart = echarts.init(chartEle.current)
         pieChart.setOption({
             title: {
-              text: 'Referer of a Website',
-              subtext: 'Fake Data',
-              left: 'center'
+                text: '',
+                subtext: ''
             },
             tooltip: {
-              trigger: 'item'
+                trigger: 'item'
             },
             legend: {
-              orient: 'vertical',
-              left: 'left'
+                orient: 'vertical',
+                left: 'left'
             },
             series: [
-              {
-                name: 'Access From',
-                type: 'pie',
-                radius: '50%',
-                data: [
-                  { value: 1048, name: 'Search Engine' },
-                  { value: 735, name: 'Direct' },
-                  { value: 580, name: 'Email' },
-                  { value: 484, name: 'Union Ads' },
-                  { value: 300, name: 'Video Ads' }
-                ],
-                emphasis: {
-                  itemStyle: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                  }
+                {
+                    name: 'Access From',
+                    type: 'pie',
+                    radius: '50%',
+                    data: [
+                        { value: 1048, name: 'Search Engine' },
+                        { value: 735, name: 'Direct' },
+                        { value: 580, name: 'Email' },
+                        { value: 484, name: 'Union Ads' },
+                        { value: 300, name: 'Video Ads' }
+                    ],
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
                 }
-              }
             ]
-          });
+        })
+        handleResize = debounce(() => {
+            pieChart && pieChart.resize()
+        }, 160)
+        pieChart && window.addEventListener('resize', () => {
+            handleResize()
+        })
+    }
+    useEffect(() => {
+        try {
+            init()
+        } catch (error) {
+            throw error
+        }
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
-        <div id="chart" ref={chartEle} style={{height: '100%', width: '100%'}}></div>
+        <div id="chart-component" style={{ height: '100%', width: '100%' }}>
+            <p className="title">投诉类型</p>
+            <div className="chart-ele" ref={chartEle}></div>
+        </div>
     )
 }
 
